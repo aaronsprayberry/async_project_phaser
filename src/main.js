b2b2c41db7f20a33303029b08b5399bcbd31f241
Aaron Sprayberry <aaronsprayberry@icloud.com>
2023-04-14 15:37:12 -0500

    added config and game scene to the game

--- src/main.js
+++ src/main.js
@@ -1,19 +1,18 @@
-import Phaser from 'phaser'
-
-import HelloWorldScene from './HelloWorldScene'
+import Phaser from "phaser";
+import GameScene from "./GameScense";
 
 const config = {
-	type: Phaser.AUTO,
-	parent: 'app',
-	width: 800,
-	height: 600,
-	physics: {
-		default: 'arcade',
-		arcade: {
-			gravity: { y: 200 },
-		},
-	},
-	scene: [HelloWorldScene],
-}
+  type: Phaser.AUTO,
+  parent: "app",
+  width: 800,
+  height: 600,
+  physics: {
+    default: "arcade",
+    arcade: {
+      gravity: { y: 300 },
+    },
+  },
+  scene: [GameScene],
+};
 
-export default new Phaser.Game(config)
+export default new Phaser.Game(config);
